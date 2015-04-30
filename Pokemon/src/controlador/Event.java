914814ec84b9c16048acd83b3bf87dcf3b04bc00
LeoAkira <leohirano95@gmail.com>
2015-04-30 20:16:50 -0300

    Update Event.java

--- Pokemon/src/controlador/Event.java
+++ Pokemon/src/controlador/Event.java
@@ -2,6 +2,5 @@ package controlador;
 
 public abstract class Event {
 	
-	abstract public void action(Treinador atual, Treinador enemy);
-	abstract public String description();
+	abstract public boolean action(Treinador atual, Treinador enemy);
 }
