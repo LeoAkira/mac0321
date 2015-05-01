038611ccf5e86cbba832e5a89ff07f8ed3e520bf
LeoAkira <leohirano95@gmail.com>
2015-05-01 00:38:56 -0300

    Update Controller.java

--- Pokemon/src/controlador/Controller.java
+++ Pokemon/src/controlador/Controller.java
@@ -1,16 +1,40 @@
 package controlador;
 
+import java.util.Scanner;
+import controlador.Batalha;
+
+
 public class Controller {
-	private EventSet es = new EventSet();
-	public void addEvent (Event c) {
-		es.add(c);
-	}
-	public void run(){
+	public void run(Treinador primeiro, Treinador segundo) {
 		Event e;
-		
-		while((e = es.getNext()) != null) {
-			e.action(null, null);
-			es.removeCurrent();
+		boolean continua = true;
+
+		while (continua) {
+			int resposta1;
+			int resposta2;
+
+			System.out.println("What will " + primeiro.name + " do?");
+			System.out.println("1: Fight");
+			System.out.println("2: Bag");
+			System.out.println("3: Pokemon");
+			System.out.println("4: Run");
+
+			Scanner scanIn = new Scanner(System.in);
+			resposta1 = scanIn.nextInt();
+			scanIn.close();
+
+			System.out.println("What will " + segundo.name + " do?");
+			System.out.println("1: Fight");
+			System.out.println("2: Bag");
+			System.out.println("3: Pokemon");
+			System.out.println("4: Run");
+
+			Scanner scanIn2 = new Scanner(System.in);
+			resposta2 = scanIn2.nextInt();
+			scanIn2.close();
+			
+			
+
 		}
 	}
 }
