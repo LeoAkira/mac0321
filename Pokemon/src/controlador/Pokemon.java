304b9ecc74f16c11325772292ee5518a46b36ac4
LeoAkira <leohirano95@gmail.com>
2015-05-03 23:40:39 -0300

    Update Pokemon.java

--- Pokemon/src/controlador/Pokemon.java
+++ Pokemon/src/controlador/Pokemon.java
@@ -6,8 +6,15 @@ public class Pokemon {
 	String name;
 	int maxHp;
 	int Hp;
-	boolean ativo;
-	String type;
+	boolean ativo = false;
 	Move[] moves;
 	boolean vivo = true;
+	public Pokemon(String name, int hp, Move[] moves) {
+		this.nickname = name;
+		this.name = name;
+		this.maxHp = hp;
+		this.Hp = hp;
+		this.moves = moves;
+	}
+	
 }
