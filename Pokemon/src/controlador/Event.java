e3e7c775c9a04ef32fba856b7a9ec0b67ce8648c
LeoAkira <leohirano95@gmail.com>
2015-05-03 23:39:30 -0300

    Update Event.java

--- Pokemon/src/controlador/Event.java
+++ Pokemon/src/controlador/Event.java
@@ -1,6 +1,13 @@
 package controlador;
 
+import java.util.Scanner;
+
 public abstract class Event {
-	
+	boolean continua;
+	Scanner scanIn;
 	abstract public boolean action(Treinador atual, Treinador enemy);
+	public Event(Treinador atual, Treinador enemy, Scanner scanIn) {
+		this.continua = true;
+		this.scanIn = scanIn;
+	}
 }
