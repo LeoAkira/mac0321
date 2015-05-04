2898717d976ce1b18b9e32f46dba42ccf96e8734
LeoAkira <leohirano95@gmail.com>
2015-05-03 23:40:20 -0300

    Update Move.java

--- Pokemon/src/controlador/Move.java
+++ Pokemon/src/controlador/Move.java
@@ -5,6 +5,11 @@ public class Move {
 	int damage;
 	int prioridade;
 	
+	public Move(String name, int damage, int prioridade) {
+		this.name = name;
+		this.damage = damage;
+		this.prioridade = prioridade;
+	}
 	void attack(Pokemon atacado){
 		atacado.Hp -= damage;
 	}
