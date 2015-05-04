1d071147072340206b5686fefdd7a094acca3c97
LeoAkira <leohirano95@gmail.com>
2015-05-04 11:10:18 -0300

    Update Treinador.java

--- Pokemon/src/controlador/Treinador.java
+++ Pokemon/src/controlador/Treinador.java
@@ -4,10 +4,12 @@ public class Treinador {
 	String name;
 	Pokemon[] pokemons;
 	Pokemon pokemonAtivo;
+	boolean selvagem;
 	public Treinador(String nome, Pokemon[] pokemons){
 		this.name = nome;
 		this.pokemons = pokemons;
 		this.pokemonAtivo = pokemons[0];
 		this.pokemons[0].ativo = true;
+		this.selvagem = false;
 	}
 }
